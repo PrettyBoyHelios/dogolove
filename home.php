c97ba713a5e627867c6bf58bf484a3cb75ec0866
jorgeluis009 <0188169@up.edu.mx>
2020-05-29 14:09:32 -0500

    updates on match logic

--- home.php
+++ home.php
@@ -1,14 +1,14 @@
 <?php
-    include ("db.php");
-    session_start();
-    if(!isset($_SESSION['userId'])){
-        header("Location: index.php");
-    }
-    $user = getUserInfo($_SESSION['userId']);
-    if (!$user->hasDog) {
-        header("Location: pet_register.php");
-    }
-    $otherProfile = getOtherProfiles($user);
+include ("db.php");
+session_start();
+if(!isset($_SESSION['userId'])){
+    header("Location: index.php");
+}
+$user = getUserInfo($_SESSION['userId']);
+if (!$user->hasDog) {
+    header("Location: pet_register.php");
+}
+$otherProfile = getOtherProfiles($user);
 ?>
 
 <!DOCTYPE html>
@@ -19,13 +19,13 @@
     <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/a81368914c.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-<?php
+    <?php
     include("menu.php");
-?>
+    ?>
     <div class="container">
         <div class="row">
             <div class="col">
@@ -48,29 +48,36 @@
         </div>
         <div class="row like-zone">
             <div class="col text-center">
-                <button class="nay-button">
-                    <i class="fas fa-times" style="color: white"></i>
-                </button>
+                <form method="">
+                    <button class="nay-button" name="nay">
+                        <i class="fas fa-times" style="color: white"></i>
+                    </button>
+                </form>
             </div>
             <div class="col text-center">
-                <button class="yay-button">
-                    <i class="fas fa-heart" style="color: white"></i>
-                </button>
+                <form method="post">
+                    <input type="hidden" name="idUser"  value="<?php echo $user->id; ?>" />       
+                    <input type="hidden" name="idOther" value="<?php echo $otherProfile->id; ?>" />
+
+                    <button class="yay-button" name="yay">
+                        <i class="fas fa-heart" style="color: white"></i>
+                    </button>
+                </form>
             </div>
         </div>
 
-        <div class="card like-zone" style="width: 100%; border-radius: 20px;">
-            <div class="row">
-                <div class="col" style="width: 100px">
-                    <img src="images/<?php echo $otherProfile->profile ?>" alt="..." class="avatar">
-                    <div class="card-body">
-                        <h5 class="card-title">Owner Info</h5>
-                        <p class="card-text"><?php echo $otherProfile->name . " " . $otherProfile->lastName ?>, 3km</p>
-                    </div>
-                </div>
+<div class="card like-zone" style="width: 100%; border-radius: 20px;">
+    <div class="row">
+        <div class="col" style="width: 100px">
+            <img src="images/<?php echo $otherProfile->profile ?>" alt="..." class="avatar">
+            <div class="card-body">
+                <h5 class="card-title">Owner Info</h5>
+                <p class="card-text"><?php echo $otherProfile->name . " " . $otherProfile->lastName ?>, 3km</p>
             </div>
         </div>
     </div>
+</div>
+</div>
 
 <?php include ("bottom.php");?>
 </body>
