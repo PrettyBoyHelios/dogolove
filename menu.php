9cb614bf2d2c1b244adb0c55350f2e7f07230799
helios <helios@polispay.org>
2020-05-29 13:48:42 -0500

    Updated nav bars

--- menu.php
+++ menu.php
@@ -10,19 +10,19 @@
                         <div class="collapse navbar-collapse" id="navbarText">
                             <ul class="navbar-nav mr-auto">
                                 <li class="nav-item active">
-                                    <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
+                                    <a class="nav-link" href="home.php"><i class="fas fa-home"></i> Home <span class="sr-only">(current)</span></a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="home.php">Explore</a>
+                                    <a class="nav-link" href="home.php"><i class="fas fa-compass"></i> Explore</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="profile.php">Profile</a>
+                                    <a class="nav-link" href="matches.php"><i class="fas fa-heart"></i> Matches</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="logout.php">Log out</a>
+                                    <a class="nav-link" href="profile.php"><i class="fas fa-dog"></i> Profile</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="test.php">Testing</a>
+                                    <a class="nav-link" href="logout.php"><i class="fas fa-sign-out-alt"></i> Log out</a>
                                 </li>
                             </ul>
                             <span class="navbar-text" style="color: #fff;">
@@ -37,17 +37,17 @@
         echo '
               <div id="headerDiv">
                   <nav class="navbar navbar-expand-lg navbar-light">
-                      <a class="navbar-brand" href="home.php">LoveDog</a>
+                      <a class="navbar-brand" href="home.php"><i class="fas fa-paw"></i> LoveDog</a>
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
                         <div class="collapse navbar-collapse" id="navbarText">
                             <ul class="navbar-nav mr-auto">
                                 <li class="nav-item active">
-                                    <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                                    <a class="nav-link" href="index.php"><i class="fas fa-home"></i> Home <span class="sr-only">(current)</span></a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="pricing.html">Pricing</a>
+                                    <a class="nav-link" href="pricing.html"><i class="fas fa-tags"></i> Pricing</a>
                                 </li>
                             </ul>
                             <span class="navbar-text" style="color: #fff;">
